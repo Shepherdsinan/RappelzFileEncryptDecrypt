ea37cef2bb50b9b9d7bbed0b431175896fc5ea2f
Shepherdsinan <65864888+Shepherdsinan@users.noreply.github.com>
2022-10-16 11:23:24 +0300

    ProjectCreated

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtEncrypt = new System.Windows.Forms.TextBox();
+            this.txtDecrypt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(97, 70);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(429, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtEncrypt
+            // 
+            this.txtEncrypt.Location = new System.Drawing.Point(97, 148);
+            this.txtEncrypt.Name = "txtEncrypt";
+            this.txtEncrypt.Size = new System.Drawing.Size(429, 20);
+            this.txtEncrypt.TabIndex = 1;
+            // 
+            // txtDecrypt
+            // 
+            this.txtDecrypt.Location = new System.Drawing.Point(97, 222);
+            this.txtDecrypt.Name = "txtDecrypt";
+            this.txtDecrypt.Size = new System.Drawing.Size(429, 20);
+            this.txtDecrypt.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(613, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(613, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Descryt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 305);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDecrypt);
+            this.Controls.Add(this.txtEncrypt);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtEncrypt;
+        private System.Windows.Forms.TextBox txtDecrypt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //public string RC4(string input, string key)
+        //{
+        //    StringBuilder result = new StringBuilder();
+        //    int x, y, j = 0;
+        //    int[] box = new int[256];
+        //    for (int i = 0; i < 256; i++)
+        //        box[i] = i;
+        //    for (int i = 0; i < 256; i++)
+        //    {
+        //        j = (key[i % key.Length] + box[i] + j) % 256;
+        //        x = box[i];
+        //        box[i] = box[j];
+        //        box[j] = x;
+        //    }
+        //    for (int i = 0; i < input.Length; i++)
+        //    {
+        //        y = i % 256;
+        //        j = (box[y] + j) % 256;
+        //        x = box[y];
+        //        box[y] = box[j];
+        //        box[j] = x;
+        //        result.Append((char)(input[i] ^ box[(box[y] + box[j]) % 256]));
+        //    }
+        //    return result.ToString();
+        //}
+
+
+            public static string Encrypt(string key, string data)
+            {
+                Encoding unicode = Encoding.Unicode;
+
+                return Convert.ToBase64String(Encrypt(unicode.GetBytes(key), unicode.GetBytes(data)));
+            }
+
+            public static string Decrypt(string key, string data)
+            {
+                Encoding unicode = Encoding.Unicode;
+
+                return unicode.GetString(Encrypt(unicode.GetBytes(key), Convert.FromBase64String(data)));
+            }
+
+            public static byte[] Encrypt(byte[] key, byte[] data)
+            {
+                return EncryptOutput(key, data).ToArray();
+            }
+
+            public static byte[] Decrypt(byte[] key, byte[] data)
+            {
+                return EncryptOutput(key, data).ToArray();
+            }
+
+            private static byte[] EncryptInitalize(byte[] key)
+            {
+                byte[] s = Enumerable.Range(0, 256)
+                  .Select(i => (byte)i)
+                  .ToArray();
+
+                for (int i = 0, j = 0; i < 256; i++)
+                {
+                    j = (j + key[i % key.Length] + s[i]) & 255;
+
+                    Swap(s, i, j);
+                }
+
+                return s;
+            }
+
+            private static IEnumerable<byte> EncryptOutput(byte[] key, IEnumerable<byte> data)
+            {
+                byte[] s = EncryptInitalize(key);
+
+                int i = 0;
+                int j = 0;
+
+                return data.Select((b) =>
+                {
+                    i = (i + 1) & 255;
+                    j = (j + s[i]) & 255;
+
+                    Swap(s, i, j);
+
+                    return (byte)(b ^ s[(s[i] + s[j]) & 255]);
+                });
+            }
+
+            private static void Swap(byte[] s, int i, int j)
+            {
+                byte c = s[i];
+
+                s[i] = s[j];
+                s[j] = c;
+            }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtEncrypt.Text = Encrypt("HongFlashMall", txtInput.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtDecrypt.Text = Decrypt("HongFlashMall", txtEncrypt.Text);
+        }
+    }
+
+}
